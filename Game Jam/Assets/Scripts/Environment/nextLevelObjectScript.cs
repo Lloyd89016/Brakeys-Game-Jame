5210f7e89c8a9009408fa4cb80e6de4fd6442672
Lloyd89016 <Lloyd89016@gmail.com>
2022-03-20 15:32:30 -0500

    Started setting up a new camera and a new part of the game

--- Game Jam/Assets/Scripts/Environment/nextLevelObjectScript.cs
+++ Game Jam/Assets/Scripts/Environment/nextLevelObjectScript.cs
@@ -5,13 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class nextLevelObjectScript : MonoBehaviour
 {
-    
+    public GameObject blackScreen;
+    private GameObject player;
+    public AudioSource loadSound;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        blackScreen.SetActive(false);
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            StartCoroutine(loadNextScene());
         }
     }
 
@@ -19,7 +28,15 @@ public class nextLevelObjectScript : MonoBehaviour
     {
         if (collision.gameObject.name == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            StartCoroutine(loadNextScene());
         }
+    }
+
+    IEnumerator loadNextScene()
+    {
+        blackScreen.SetActive(true);
+        loadSound.Play();
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Game Jam/Assets/Scripts/camera/fallCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallCamera : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 cameraOffset;
+    private Vector3 target;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");        
+    }
+
+    void Update()
+    {
+        target = player.transform.position + cameraOffset;
+
+        transform.position = target;   
+    }
+}
